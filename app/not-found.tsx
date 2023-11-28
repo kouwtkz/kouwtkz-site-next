245ea4b46074201a8ac16b0ae157918ca2d51d8d
kouwtkz <kouwtkz@cottonwind.com>
2023-11-28 15:14:34 +0000

    404の実装

---
+++ app/not-found.tsx
@@ -0,0 +1,14 @@
+"use client";
+import React from "react";
+
+// データが存在しないときの画面
+const NotFound = () => {
+  return (
+    <div className="pt-24">
+      <h1 className="text-main text-center text-4xl my-12 font-LuloClean mb-3">404 not found</h1>
+      <h4 className="text-main-soft text-center text-xl font-bold">ページが見つかりませんでした</h4>
+    </div>
+  );
+};
+
+export default NotFound;
