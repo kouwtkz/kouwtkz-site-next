38fde1d8a2819d64bccee637859e60d597b4c882
kouwtkz <kouwtkz@cottonwind.com>
2023-11-29 06:50:20 +0000

    トップページにお知らせを簡易的に載せてみた

--- app/components/client/TopPage.tsx
+++ app/components/client/TopPage.tsx
@@ -2,6 +2,7 @@
 
 import loaderSet from "@/app/lib/loaderSet";
 import { ImageDataInfo } from "@/media/scripts/media";
+import { Post } from "@prisma/client";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
@@ -9,14 +10,19 @@ import Twemoji from "react-twemoji";
 type TopPageProps = {
   topImage?: ImageDataInfo;
   isStatic?: boolean;
+  topPosts?: Array<Post>;
 };
 
-const TopPage: React.FC<TopPageProps> = ({ topImage, isStatic = false }) => {
+const TopPage: React.FC<TopPageProps> = ({
+  topImage,
+  isStatic = false,
+  topPosts = [],
+}) => {
   return (
     <>
       {topImage ? (
         <Image
-        src={`${topImage.imageUrl}`}
+          src={`${topImage.imageUrl}`}
           loader={loaderSet(isStatic)}
           alt={topImage.name || topImage.src}
           width={topImage.size?.width}
@@ -25,17 +31,31 @@ const TopPage: React.FC<TopPageProps> = ({ topImage, isStatic = false }) => {
         />
       ) : null}
       <Twemoji options={{ className: "emoji" }}>
-        <main className="p-8">
-          <div className="text-4xl [&>*]:m-4">
-            <h1>はろはろめぇめぇ</h1>
-            <div>
-              <Link href="gallery">イラスト</Link>
+        <main className="pb-8">
+          <div className="my-8">
+            <div className="text-4xl [&>*]:m-4 mb-8">
+              <h1>はろはろめぇめぇ</h1>
+              <div>
+                <Link href="gallery">イラスト</Link>
+              </div>
+              <div>
+                <Link href="character">キャラクター</Link>
+              </div>
+              <div>
+                <Link href="blog">ブログ</Link>
+              </div>
             </div>
+          </div>
+          <div className="my-8">
+            <h3 className="text-2xl my-4">お知らせ</h3>
             <div>
-              <Link href="character">キャラクター</Link>
-            </div>
-            <div>
-              <Link href="blog">ブログ</Link>
+              {topPosts.map((post) => (
+                <>
+                  <div className="m-1">
+                    <Link href={`/blog/post/${post.postId}`}>{post.title}</Link>
+                  </div>
+                </>
+              ))}
             </div>
           </div>
         </main>
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,24 @@
+import prisma from "@/app/lib/prisma";
 import TopPage from "@/app/components/client/TopPage";
 import { isStatic } from "@/siteData/site";
 import { imageList } from "@/media/scripts/MediaData";
 
-export default function Page() {
+export default async function Page() {
+  const topPosts = await prisma.post.findMany({
+    where: {
+      category: "お知らせ",
+    },
+    orderBy: {
+      date: "desc"
+    },
+    take: 3
+  });
   return (
     <>
       <TopPage
         isStatic={isStatic}
         topImage={imageList.find((image) => image.topImage)}
+        topPosts={topPosts}
       />
     </>
   );
