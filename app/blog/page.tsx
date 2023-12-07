cffe2c6c93047fdc00ae942e8d954153e4e009f9
kouwtkz <kouwtkz@cottonwind.com>
2023-12-07 15:23:49 +0000

    ブログ一覧ページの設計も他の水準に合わせた

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -4,6 +4,7 @@ import isStatic from "@/app/components/System/isStatic.mjs";
 import Link from "next/link";
 import { redirect } from "next/navigation";
 import CheckPostId from "./CheckPostId";
+import { Suspense } from "react";
 
 export default async function BlogPage({
   params,
@@ -12,46 +13,42 @@ export default async function BlogPage({
   params: { slug: string };
   searchParams: { [key: string]: string | undefined };
 }) {
-  const renderList = [];
-  const postId =
+  const redirectPostId =
     !isStatic && searchParams.postId ? searchParams.postId : undefined;
-  if (postId) {
-    redirect(`/blog/post/${postId}`);
-  } else if (isStatic) {
-    renderList.push(<CheckPostId />);
-  }
-  const q = !isStatic && searchParams.q ? searchParams.q : undefined;
+  if (redirectPostId) redirect(`/blog/post/${redirectPostId}`);
   // 投稿一覧取得
   const posts = await getPosts({ max: 10 });
 
-  // 投稿がない場合
-  if (posts.length === 0) {
-    renderList.push(<div className="text-center">投稿はありません</div>);
-    return renderList;
-  }
-  renderList.push(
+  return (
     <div>
+      {isStatic ? (
+        <Suspense>
+          <CheckPostId />
+        </Suspense>
+      ) : null}
       <h2 className="text-4xl font-LuloClean text-center text-main pt-8 mb-12">
         MINI BLOG
       </h2>
       <div className="w-[100%] md:w-[80%] mx-auto">
-        {posts.map((post, index) => {
-          return (
-            <div key={index} className="m-4">
-              <h3 className="text-2xl text-main-dark font-bold inline-block m-4">
-                <Link href={`/blog/post/${post.postId}`}>{post.title}</Link>
-              </h3>
-              <span className="underline">
-                <Link href={`/blog/?q=%23${post.category}`}>
-                  {post.category}
-                </Link>
-              </span>
-            </div>
-          );
-        })}
+        {posts.length > 0 ? (
+          posts.map((post, index) => {
+            return (
+              <div key={index} className="m-4">
+                <h3 className="text-2xl text-main-dark font-bold inline-block m-4">
+                  <Link href={`/blog/post/${post.postId}`}>{post.title}</Link>
+                </h3>
+                <span className="underline">
+                  <Link href={`/blog/?q=%23${post.category}`}>
+                    {post.category}
+                  </Link>
+                </span>
+              </div>
+            );
+          })
+        ) : (
+          <div className="text-center">投稿はありません</div>
+        )}
       </div>
     </div>
   );
-
-  return renderList;
 }
