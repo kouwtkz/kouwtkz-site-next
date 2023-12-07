7c29ffe221930ef224f86daa02bdb1e7b7c0733d
kouwtkz <kouwtkz@cottonwind.com>
2023-12-07 11:51:46 +0000

    サウンドページのクリック範囲を適正にした

--- app/sound/client.tsx
+++ app/sound/client.tsx
@@ -7,7 +7,6 @@ type SoundPageProps = {
   soundAlbum: SoundAlbumType | null;
 };
 
-
 const SoundPage: React.FC<SoundPageProps> = ({ soundAlbum }) => {
   return (
     <div>
@@ -15,19 +14,22 @@ const SoundPage: React.FC<SoundPageProps> = ({ soundAlbum }) => {
         <h1 className="font-LuloClean text-4xl text-main pt-8 mb-8">
           SOUND ROOM
         </h1>
-        <div onClick={() => AudioStop()} className="cursor-pointer text-main">停止</div>
+        <div onClick={() => AudioStop()} className="cursor-pointer text-main">
+          停止
+        </div>
         {soundAlbum?.playlist?.map((album, i) => (
           <div key={i} className="my-6">
             <h3 className="my-4 text-main-deep">{album.title}</h3>
             {album.list.map((sound, i) => (
-              <div
-                key={i}
-                className="cursor-pointer my-1"
-                onClick={(e) => {
-                  AudioPlay(sound.src);
-                }}
+              <div key={i} className="my-1">
+                <span
+                  className="cursor-pointer px-2"
+                  onClick={(e) => {
+                    AudioPlay(sound.src);
+                  }}
                 >
-                {sound.title}
+                  {sound.title}
+                </span>
               </div>
             ))}
           </div>
