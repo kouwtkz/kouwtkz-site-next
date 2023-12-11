e005e3d2b4f712da87d5596d14803b4a4e1502ae
kouwtkz <kouwtkz@cottonwind.com>
2023-12-11 17:34:05 +0000

    ブログのフロートボタンの調整

--- app/blog/Fixed.tsx
+++ app/blog/Fixed.tsx
@@ -10,16 +10,14 @@ type props = { isStatic: boolean; max?: number };
 export default function Fixed({ isStatic, max }: props) {
   return (
     <div className="fixed z-30 right right-0 bottom-0">
-      <div className="flex flex-row m-2">
-        <div className="flex flex-col-reverse">
-          <PagingArea max={max} />
-        </div>
+      <div className="flex flex-wrap justify-end m-2 ml-36">
+        <PagingArea max={max} />
         {!isStatic ? (
-          <div className="flex flex-col-reverse">
+          <div className="flex flex-row">
             <SearchArea />
+            <PostButton />
           </div>
         ) : null}
-        <div className="flex flex-col">{!isStatic ? <PostButton /> : null}</div>
       </div>
     </div>
   );
