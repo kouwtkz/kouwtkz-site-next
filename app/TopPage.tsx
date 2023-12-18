938017d9d9313a8b7376829f3189b077373b0b36
kouwtkz <kouwtkz@cottonwind.com>
2023-12-18 22:01:31 +0900

    トップページの画像が時間経過でランダムに切り替わるようにした

--- app/TopPage.tsx
+++ app/TopPage.tsx
@@ -4,10 +4,12 @@ import loaderSet from "@/app/lib/loaderSet";
 import { MediaImageItemType } from "@/app/media/image/MediaImageType";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useServerState } from "./components/System/ServerState";
 import MultiParser from "./components/functions/MultiParser";
 import Notice from "./blog/Notice";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+import fadein from "./styles/transitions/fadein.module.scss";
 
 type TopPageProps = {
   topImages?: MediaImageItemType[];
@@ -15,24 +17,44 @@ type TopPageProps = {
 
 export default function TopPage({ topImages = [] }: TopPageProps) {
   const { isStatic } = useServerState();
-  const topImage = topImages[Math.floor(Math.random() * topImages.length)];
+  const [topImageState, setTopImage] = useState<MediaImageItemType>();
+  const imageRnd = (images: MediaImageItemType[]) =>
+    images[Math.floor(Math.random() * images.length)];
+  const currentTopImage = useRef<MediaImageItemType>(imageRnd(topImages));
+  if (topImageState) currentTopImage.current = topImageState;
+  const topImage = currentTopImage.current;
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTopImage(
+        imageRnd(
+          topImages.filter((image) => image.src !== currentTopImage.current.src)
+        )
+      );
+    }, 10000);
+    return () => clearInterval(timer);
+  });
+
   return (
     <>
-      {topImage ? (
-        <Image
-          src={`${topImage.innerURL}`}
-          loader={loaderSet(isStatic, topImage.path)}
-          suppressHydrationWarning={isStatic}
-          alt={topImage.name || topImage.src}
-          width={topImage.info?.width}
-          height={topImage.info?.height}
-          loading="eager"
-          unoptimized={isStatic}
-          className="w-[100%] h-80"
-        />
-      ) : (
-        <div className="w-[100%] h-80" />
-      )}
+      <TransitionGroup className="wrapper h-80 relative">
+        <CSSTransition
+          key={currentTopImage.current.src}
+          classNames={fadein}
+          timeout={750}
+        >
+          <Image
+            src={`${topImage.innerURL}`}
+            loader={loaderSet(isStatic, topImage.path)}
+            suppressHydrationWarning={isStatic}
+            alt={topImage.name || topImage.src}
+            width={topImage.info?.width}
+            height={topImage.info?.height}
+            loading="eager"
+            unoptimized={isStatic}
+            className="w-[100%] h-[100%] absolute"
+          />
+        </CSSTransition>
+      </TransitionGroup>
       <main className="pb-8">
         <div className="my-8">
           <div className="text-4xl [&>*]:m-4 mb-8">
