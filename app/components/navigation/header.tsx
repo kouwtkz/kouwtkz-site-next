a9884c56e2d1c4c1a1b682ed46f41e393682f8e7
kouwtkz <kouwtkz@cottonwind.com>
2023-12-24 18:50:27 +0900

    ヘッダーの高さをあわせた

--- app/components/navigation/header.tsx
+++ app/components/navigation/header.tsx
@@ -13,8 +13,8 @@ export default function Header({ site }: HeaderProps) {
   return (
     <header className="fixed z-10 text-center w-[100vw]">
       <MenuButton className="block md:hidden absolute right-2 m-1" />
-      <div className="container mt-4 mb-2 inline-block">
-        <Link href="/" className="inline-block px-2 py-1">
+      <div className="container my-3 inline-block">
+        <Link href="/" className="inline-block px-2 h-10">
           <div id="siteTitle" className="text-3xl font-MochiyPopOne text-main">
             {site.title}
           </div>
