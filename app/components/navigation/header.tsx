cf60c282e122af49c1b96e1167e52d3cc891d337
kouwtkz <kouwtkz@cottonwind.com>
2023-12-28 23:59:13 +0900

    ヘッダーの一部をメモ化した

--- app/components/navigation/header.tsx
+++ app/components/navigation/header.tsx
@@ -2,13 +2,54 @@
 
 import React, { useState } from "react";
 import Link from "next/link";
-import { SiteProps } from "@/app/site/SiteData.mjs";
+import { SiteProps, SiteMenuProps } from "@/app/site/SiteData.mjs";
 import MenuButton from "@/app/components/svg/MenuButton";
 import Breakcrumb from "./breadcrumb";
 
 type HeaderProps = {
   site: SiteProps;
 };
+
+const SiteMenu = React.memo(function SiteMenu({
+  nav,
+  setIsOpen,
+}: {
+  nav: SiteMenuProps[];
+  setIsOpen: Function;
+}) {
+  return (
+    <div className="absolute right-0 bg-white bg-opacity-90 flex font-LuloClean flex-wrap justify-center md:flex-col sm:text-right">
+      {nav.map((item, i) => (
+        <Link
+          key={i}
+          href={item.url}
+          className="min-w-48 px-4 py-2 hover:bg-main-pale-fluo"
+          onClick={() => {
+            setTimeout(() => {
+              setIsOpen(false);
+            }, 350);
+          }}
+        >
+          {item.name}
+        </Link>
+      ))}
+    </div>
+  );
+});
+const SiteTitle = React.memo(function SiteTitle({ title }: { title: string }) {
+  return (
+    <div className="container my-3 inline-block">
+      <Link href="/" className="inline-block px-2 h-10">
+        <div
+          id="siteTitle"
+          className="text-2xl sm:text-3xl leading-10 font-MochiyPopOne text-main"
+        >
+          {title}
+        </div>
+      </Link>
+    </div>
+  );
+});
 
 export default function Header({ site }: HeaderProps) {
   const [isOpen, setIsOpen] = useState(false);
@@ -23,33 +64,11 @@ export default function Header({ site }: HeaderProps) {
         }}
         className="block absolute select-none right-0 h-14 m-1 opacity-80"
       />
-      <div className="container my-3 inline-block">
-        <Link href="/" className="inline-block px-2 h-10">
-          <div
-            id="siteTitle"
-            className="text-2xl sm:text-3xl leading-10 font-MochiyPopOne text-main"
-          >
-            {site.title}
-          </div>
-        </Link>
-      </div>
-      <div className="absolute top-0 m-0 h-[100%] w-[100%] bg-background-top opacity-90 -z-10"></div>
-      {isOpen && site ? (
-        <div className="absolute right-0 bg-white bg-opacity-90 flex font-LuloClean flex-wrap justify-center md:flex-col sm:text-right">
-          {site.menu?.nav?.map((item, i) => (
-            <Link
-              key={i}
-              href={item.url}
-              className="min-w-48 px-4 py-2 hover:bg-main-pale-fluo"
-              onClick={() => {
-                setTimeout(() => {
-                  setIsOpen(false);
-                }, 350);
-              }}
-            >
-              {item.name}
-            </Link>
-          ))}
+      <SiteTitle title={site.title} />
+      <div className="absolute top-0 m-0 h-[100%] w-[100%] bg-background-top opacity-90 -z-10" />
+      {site.menu?.nav ? (
+        <div className={isOpen ? "" : "hidden"}>
+          <SiteMenu setIsOpen={setIsOpen} nav={site.menu.nav} />
         </div>
       ) : null}
     </header>
