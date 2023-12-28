e4b5dec69de5c38ccb5863a098884eedbd3bbc39
kouwtkz <kouwtkz@cottonwind.com>
2023-12-28 17:02:42 +0900

    ヘッダーの横幅をvwではなく%にした

--- app/components/navigation/header.tsx
+++ app/components/navigation/header.tsx
@@ -12,9 +12,9 @@ type HeaderProps = {
 
 export default function Header({ site }: HeaderProps) {
   return (
-    <header className="fixed z-10 text-center w-[100vw]">
+    <header className="fixed z-10 text-center w-[100%]">
       <Breakcrumb className="absolute left-0 m-1 h-14 w-14" />
-      <MenuButton className="block absolute right-2 h-14 m-1 opacity-80 md:hidden" />
+      <MenuButton className="block absolute right-0 h-14 m-1 opacity-80" />
       <div className="container my-3 inline-block">
         <Link href="/" className="inline-block px-2 h-10">
           <div id="siteTitle" className="text-3xl font-MochiyPopOne text-main">
