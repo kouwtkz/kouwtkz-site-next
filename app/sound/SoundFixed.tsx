3e0453be66467a0913d7a3db4196e8b7ce9c1da0
kouwtkz <kouwtkz@cottonwind.com>
2023-12-31 20:13:40 +0900

    よこにした

--- app/sound/SoundFixed.tsx
+++ app/sound/SoundFixed.tsx
@@ -9,7 +9,7 @@ export default function SoundFixed() {
   const { Play, Pause, Stop, paused } = useSoundPlayer();
   return (
     <div className="fixed z-30 left-0 bottom-0 select-none">
-      <div className="flex flex-col m-2 [&>*]:m-1">
+      <div className="flex m-2 [&>*]:m-1">
         <PlayPauseButton
           paused={paused}
           className="fill-main-soft cursor-pointer"
