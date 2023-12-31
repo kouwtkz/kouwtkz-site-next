006f718669520724188ae9e1782b9e7e79237ff3
kouwtkz <kouwtkz@cottonwind.com>
2023-12-31 18:09:49 +0900

    無用なターミナル出力の削除

--- app/data/sound.json/route.ts
+++ app/data/sound.json/route.ts
@@ -1,6 +1,5 @@
 import { NextResponse } from "next/server"
 import { soundAlbums } from "@/app/sound/MediaSoundData.mjs";
-console.log(soundAlbums);
 const soundAlbum = soundAlbums.find(album => album.name === "sound") || null;
 
 export async function GET() {
