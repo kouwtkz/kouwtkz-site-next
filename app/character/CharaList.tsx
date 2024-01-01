d84eb20e748136883c6f2ca8722bdd0515c13d9c
kouwtkz <kouwtkz@cottonwind.com>
2024-01-02 02:37:30 +0900

    キャラクター一覧のUI調整

--- app/character/CharaList.tsx
+++ app/character/CharaList.tsx
@@ -7,18 +7,18 @@ import { useCharaState } from "./CharaState";
 export default function CharaList() {
   const { charaList } = useCharaState();
   return (
-    <div className="flex flex-row flex-wrap justify-center content-around">
+    <div className="flex flex-row flex-wrap justify-center content-around w-[90%] max-w-4xl mx-auto">
       {charaList.map((chara, index) => {
         return (
           <Link
             key={index}
-            className="block p-4 w-[50%] md:w-[30%] hover:bg-main-pale-fluo hover:text-main-dark"
+            className="block p-4 w-[50%] sm:w-[33%] hover:bg-main-pale-fluo hover:text-main-dark"
             href={`character/${chara.id}`}
           >
             {chara.media?.image ? (
               <ImageMeeThumbnail
                 imageItem={chara.media.image}
-                className="block mx-auto my-2 max-w-[82%]"
+                className="block mx-auto my-2 max-w-[90%]"
               />
             ) : null}
             <div className="text-center text-xl md:text-2xl">{chara.name}</div>
