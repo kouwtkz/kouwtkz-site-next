1af62e2c52940cff9f8ef2875c9d21aa78409b30
kouwtkz <kouwtkz@cottonwind.com>
2024-01-07 14:35:28 +0900

    インページジャンプが見づらすぎるので見やすさ優先で透かし背景を入れた

--- app/components/navigation/InPageMenu.tsx
+++ app/components/navigation/InPageMenu.tsx
@@ -14,24 +14,24 @@ export default function InPageMenu({
   list?: InPageRefObject[];
 }) {
   const [x, y] = useScroll();
+  const jy = y + 64;
   const firstTop = list.length > 0 ? list[0].ref.current?.offsetTop || 0 : 0;
   return (
-    <div className="fixed z-10 right-0 bottom-0 mb-4 pr-4">
+    <div className="fixed z-10 right-0 bottom-0 mb-2 pr-2">
       {list.map((item, i) => {
         const elm = list[i].ref.current;
         const top = (elm?.offsetTop || 0) - firstTop;
         const bottom = top + (elm?.offsetHeight || 0);
-        const currentMode = top <= y && y < bottom;
+        const currentMode = top <= jy && jy < bottom;
         return (
           <div
             key={i}
             className={
-              "font-LuloClean m-1 text-left text-base sm:text-lg cursor-pointer " +
-              (currentMode ? "text-main-deep hover:text-dark" : "text-main-soft hover:text-main")
+              "bg-white bg-opacity-70 font-LuloClean px-2 py-1 text-left text-base sm:text-xl font-black cursor-pointer " +
+              (currentMode
+                ? "text-main-dark hover:text-main-grayish"
+                : "text-main-strong hover:text-main-deep")
             }
-            style={{
-              textShadow: "white 0 2px 1px",
-            }}
             onClick={() => {
               scrollTo({ top, behavior: "smooth" });
             }}
