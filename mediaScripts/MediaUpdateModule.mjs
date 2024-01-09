27a95619ef5d425b19d43decb67b1ff24ea11eed
kouwtkz <kouwtkz@cottonwind.com>
2024-01-10 04:08:52 +0900

    非同期外し

--- mediaScripts/MediaUpdateModule.mjs
+++ mediaScripts/MediaUpdateModule.mjs
@@ -2,7 +2,7 @@
 
 const ROOT = process.env.ROOT || "";
 import RunProcess from '../scripts/RunProcess.mjs';
-export async function MediaUpdate() {
+export function MediaUpdate() {
   const cd = ROOT ? `cd ${ROOT} && ` : "";
   return RunProcess(cd + "node -r dotenv/config ./mediaScripts/MediaUpdate.mjs", true)
 }
