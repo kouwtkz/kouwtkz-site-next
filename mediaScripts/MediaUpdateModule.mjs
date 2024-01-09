9cf7a07e513bd0d6ce52070b58ab8ac39cbd69d8
kouwtkz <kouwtkz@cottonwind.com>
2024-01-10 04:07:19 +0900

    子プロセス実行プログラムを同期型にして、出力文字コードを簡易的に設定できるようにした

--- mediaScripts/MediaUpdateModule.mjs
+++ mediaScripts/MediaUpdateModule.mjs
@@ -1,6 +1,8 @@
 // @ts-check
 
+const ROOT = process.env.ROOT || "";
 import RunProcess from '../scripts/RunProcess.mjs';
 export async function MediaUpdate() {
-  return await RunProcess("node -r dotenv/config ./mediaScripts/MediaUpdate.mjs")
+  const cd = ROOT ? `cd ${ROOT} && ` : "";
+  return RunProcess(cd + "node -r dotenv/config ./mediaScripts/MediaUpdate.mjs", true)
 }
