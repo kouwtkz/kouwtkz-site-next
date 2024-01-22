c7a47d21be1dc69d4f10dd2338ecd3a5487a800a
kouwtkz <kouwtkz@cottonwind.com>
2024-01-23 04:32:15 +0900

    画像のタグ編集機能の実装など

--- app/gallery/ImageViewer.tsx
+++ app/gallery/ImageViewer.tsx
@@ -11,11 +11,23 @@ import { useRouter } from "next/navigation";
 import { BlogDateOptions as opt } from "@/app/components/System/DateTimeFormatOptions";
 import ImageMee from "../components/image/ImageMee";
 import CloseButton from "../components/svg/button/CloseButton";
-import { EmbedNode, useEmbedState } from "../context/embed/EmbedState";
+import { EmbedNode } from "../context/embed/EmbedState";
 import { useServerState } from "../components/System/ServerState";
-import axios from "axios";
-import toast from "react-hot-toast";
 import { MediaImageItemType } from "@/mediaScripts/MediaImageDataType";
+import ImageEditForm from "./ImageEditForm";
+export const eventTags = [
+  { value: "spring", name: "🌸春" },
+  { value: "summer", name: "🌻夏" },
+  { value: "autumn", name: "🍂秋" },
+  { value: "winter", name: "⛄冬" },
+  { value: "valentine", name: "🍫バレンタインデー" },
+  { value: "easter", name: "🐰イースター" },
+  { value: "halloween", name: "🎃ハロウィン" },
+  { value: "christmas", name: "🎄クリスマス" },
+  { value: "yosonoko", name: "🎨よその子" },
+  { value: "birthday", name: "🎂誕生日" },
+  { value: "myBirthday", name: "🎂自分の誕生日" },
+];
 
 const body = typeof window === "object" ? document?.body : null;
 const bodyLock = (m: boolean) => {
@@ -62,13 +74,10 @@ function ImageViewerWindow() {
     useImageViewer();
   const search = useSearchParams();
   const { imageItemList } = useMediaImageState();
-  const charaData = useCharaState();
-  const { data: embedData } = useEmbedState();
-  const { isServerMode } = useServerState();
+  const { charaList } = useCharaState();
   const [backCheck, setBackCheck] = useState(false);
   const imageParam = search.get("image");
-  const { setImageFromUrl } = useMediaImageState();
-  const refForm = useRef<HTMLFormElement>(null);
+  const { isServerMode } = useServerState();
 
   const backAction = () => {
     router.back();
@@ -100,111 +109,13 @@ function ImageViewerWindow() {
       ? image.src.startsWith(image.name)
       : true;
 
-  const sendUpdate = async (image: MediaImageItemType) => {
-    const { album, resized, resizeOption, URL, ..._image } = image;
-    const res = await axios.patch("/gallery/send", {
-      ..._image,
-      albumDir: album?.dir,
-    });
-    if (res.status === 200) {
-      toast("更新しました！", {
-        duration: 2000,
-      });
-      setImageFromUrl();
-    }
-  };
-
-  const onToggleEdit = async () => {
-    if (editMode && image) {
-      const form = refForm.current;
-      let sendFlag = false;
-      if (form) {
-        form.test;
-        const fmData = new FormData(form);
-        const data = Object.fromEntries(fmData);
-        const ftData = Object.entries(data).filter(([k, v]) => {
-          switch (k) {
-            case "time":
-              return image.time?.getTime() !== new Date(String(v)).getTime();
-            default:
-              return (image[k] || "") !== v;
-          }
-        });
-        if (ftData.length > 0) sendFlag = true;
-        ftData.forEach(([k, v]) => {
-          switch (k) {
-            case "time":
-              image.time = new Date(String(v));
-              break;
-            default:
-              image[k] = v;
-              break;
-          }
-        });
-      }
-      if (sendFlag) sendUpdate(image);
-    }
-    toggleEditMode();
-  };
-
   const infoCmp = (image: MediaImageItemType) => {
     if (!image.album?.visible?.info) return <></>;
     return (
       <div className="window info">
-        {editMode ? (
-          <form
-            ref={refForm}
-            onSubmit={(e) => {
-              onToggleEdit();
-              e.preventDefault();
-            }}
-            className="[&>*]:block [&>*]:my-6"
-          >
-            <label>
-              タイトル
-              <input
-                className="rounded-none w-[100%] mx-1 px-1 text-xl md:text-2xl text-main-dark"
-                title="タイトル"
-                type="text"
-                name="name"
-                defaultValue={image.name}
-              />
-            </label>
-            <label>
-              説明文
-              <textarea
-                className="rounded-none w-[100%] mx-1 px-1 min-h-[8rem] text-lg md:text-xl"
-                title="説明文"
-                name="description"
-                defaultValue={image.description}
-              />
-            </label>
-            <label>
-              リンク
-              <input
-                className="rounded-none w-[100%] mx-1 px-1 text-lg md:text-xl"
-                title="リンク"
-                type="text"
-                name="link"
-                defaultValue={image.link}
-              />
-            </label>
-            <label>
-              投稿時間
-              <input
-                className="rounded-none w-[100%] mx-1 px-1 text-lg md:text-xl"
-                title="リンク"
-                type="datetime-local"
-                name="time"
-                defaultValue={image.time
-                  ?.toLocaleString("sv-SE", { timeZone: "JST" })
-                  .replace(" ", "T")}
-              />
-            </label>
-          </form>
-        ) : (
-          <>
-            <div className="text-center md:text-left">
+        <div className="text-center md:text-left">
+          {editMode ? null : (
+            <>
               {image.album.visible.title &&
               (image.album.visible.filename || !titleEqFilename) ? (
                 <h2 className="mx-1 my-8 text-center text-2xl md:text-3xl font-MochiyPopOne text-main-deep break-all">
@@ -218,35 +129,54 @@ function ImageViewerWindow() {
                   {image.description}
                 </MultiParser>
               </div>
-              <div className="mb-8 text-2xl">
-                {charaData.charaList
+              <div className="mb-8 text-xl">
+                {charaList
                   .filter((chara) =>
-                    image.tags?.find((tag) => tag === chara.id)
+                    image.tags?.some((tag) => chara.id === tag)
                   )
-                  .map((chara, i) => (
-                    <Link
-                      className="mx-2 my-1 text-xl whitespace-nowrap inline-block"
-                      href={`/character/${chara.id}`}
-                      onClick={() => {
-                        onClose();
-                        return true;
-                      }}
-                      key={i}
-                    >
-                      {chara?.media?.icon ? (
-                        <ImageMee
-                          imageItem={chara?.media?.icon}
-                          mode="icon"
-                          width={40}
-                          height={40}
-                          className="charaIcon text-3xl mr-1"
-                        />
-                      ) : (
-                        <></>
-                      )}
-                      <span className="align-middle">{chara.name}</span>
-                    </Link>
-                  ))}
+                  .map((chara, i) => {
+                    return (
+                      <Link
+                        className="mx-2 my-1 whitespace-nowrap inline-block"
+                        href={`/character/${chara.id}`}
+                        onClick={() => {
+                          onClose();
+                          return true;
+                        }}
+                        key={i}
+                      >
+                        {chara?.media?.icon ? (
+                          <ImageMee
+                            imageItem={chara.media.icon}
+                            mode="icon"
+                            width={40}
+                            height={40}
+                            className="charaIcon text-3xl mr-1"
+                          />
+                        ) : (
+                          <></>
+                        )}
+                        <span className="align-middle">{chara.name}</span>
+                      </Link>
+                    );
+                  })}
+                {image.tags
+                  ?.filter((tag) =>
+                    eventTags.some(({ value }) => value === tag)
+                  )
+                  .map((tag, i) => {
+                    const item = eventTags.find(({ value }) => value === tag);
+                    if (!item) return item;
+                    return (
+                      <MultiParser
+                        key={i}
+                        only={{ toTwemoji: true }}
+                        className="mx-2 my-1 whitespace-nowrap inline-block text-main-dark [&_.emoji]:mr-1"
+                      >
+                        {item.name}
+                      </MultiParser>
+                    );
+                  })}
               </div>
               {image.link ? (
                 <div className="text-xl">
@@ -261,16 +191,17 @@ function ImageViewerWindow() {
               ) : (
                 <></>
               )}
-            </div>
-            {image.time ? (
-              <div className="m-4 mr-8 text-main-grayish text-right">
-                {image.time.toLocaleString("ja", opt)}
-              </div>
-            ) : (
-              <></>
-            )}
-          </>
-        )}
+              {image.time ? (
+                <div className="m-4 mr-8 text-main-grayish text-right">
+                  {image.time.toLocaleString("ja", opt)}
+                </div>
+              ) : (
+                <></>
+              )}
+            </>
+          )}
+          {isServerMode ? <ImageEditForm image={image} /> : null}
+        </div>
       </div>
     );
   };
@@ -293,18 +224,6 @@ function ImageViewerWindow() {
               e.stopPropagation();
             }}
           />
-          {isServerMode ? (
-            <>
-              <button
-                title="編集"
-                type="button"
-                className="absolute right-0 bottom-0 z-50 m-2 w-12 h-12 text-2xl rounded-full p-0"
-                onClick={onToggleEdit}
-              >
-                {editMode ? "✓" : "🖊"}
-              </button>
-            </>
-          ) : null}
           <div className="window modal z-30 font-KosugiMaru">
             <div className="preview">
               {image.embed ? (
