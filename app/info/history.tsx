bcf5396f5b86423d8cf64d80e8c6b568d28cdf4e
kouwtkz <kouwtkz@cottonwind.com>
2024-02-26 21:49:19 +0900

    Gitの更新履歴の詳細を閉じれるようにしたのとエンターキーでも開閉できるようにした

--- app/info/history.tsx
+++ app/info/history.tsx
@@ -4,13 +4,32 @@ import { useState } from "react";
 import { useGitState } from "../context/git/gitState";
 import { reducedGitItemType } from "../context/git/gitType";
 import { MdSection } from "./Section";
+import { GoTriangleDown, GoTriangleRight } from "react-icons/go";
 
 function GitlogItem({ item }: { item: reducedGitItemType }) {
   const [readMore, setReadMore] = useState(false);
+  const handleToggle = () => {
+    setReadMore(!readMore);
+  };
   return (
     <>
-      <div className="mr-1">{item.date}:</div>
       <div>
+        <span
+          tabIndex={0}
+          className="cursor-pointer inline-flex items-center"
+          onClick={handleToggle}
+          onKeyDown={(e) => {
+            if (e.code === "Enter") handleToggle();
+          }}
+        >
+          <span className="text-main-dark-grayish">
+            {readMore ? <GoTriangleDown /> : <GoTriangleRight />}
+          </span>
+          <span>{item.date}</span>
+        </span>
+      </div>
+      <div className="mr-1">:</div>
+      <div className="flex-1">
         {readMore ? (
           item.messages.map((m, i) => <p key={i}>{m}</p>)
         ) : (
@@ -20,9 +39,7 @@ function GitlogItem({ item }: { item: reducedGitItemType }) {
               {item.messages.length > 1 ? (
                 <span
                   className="ml-1 underline cursor-pointer"
-                  onClick={() => {
-                    setReadMore(true);
-                  }}
+                  onClick={handleToggle}
                 >
                   {"他" + (item.messages.length - 1) + "件"}
                 </span>
@@ -43,9 +60,11 @@ export function HistoryPage() {
         <details className="">
           <summary className="mx-auto max-w-fit">
             Gitの更新履歴
-            <span className="text-sm ml-2">
-              (最終更新:<span className="ml-1">{log[0].date}</span>)
-            </span>
+            {log[0] ? (
+              <span className="text-sm ml-2">
+                (最終更新:<span className="ml-1">{log[0].date}</span>)
+              </span>
+            ) : null}
           </summary>
           <ul className="text-left max-w-2xl">
             {log.map((item, i) => (
