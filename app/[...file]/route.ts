164dba4515cda0f130c5399c2012d86d64679316
kouwtkz <kouwtkz@cottonwind.com>
2024-02-27 03:00:32 +0900

    コンテンツの動的パスの取得に_dataディレクトリも含むようにした

--- app/[...file]/route.ts
+++ app/[...file]/route.ts
@@ -1,15 +1,28 @@
 import isStatic from "@/app/components/System/isStatic.mjs";
 export const dynamic = isStatic ? "auto" : "force-dynamic";
-import { readFileSync } from "fs";
+import { readFileSync, existsSync } from "fs";
 import { contentType } from "mime-types"
 import { NextRequest } from "next/server";
 import { extname, resolve } from "path";
 const ROOT = process.env.ROOT || "";
 
+function notfoundResponse() {
+  return new Response("file not found", {
+    headers: {
+      "Content-Type": "text/plain",
+    },
+    status: 404
+  });
+}
+
 export async function GET(req: NextRequest) {
   const pathname = decodeURI(req.nextUrl.pathname);
   try {
-    const filePath = resolve(`${ROOT}/public${pathname}`);
+    let filePath = resolve(`${ROOT}/public${pathname}`);
+    if (!existsSync(filePath)) {
+      filePath = resolve(`${ROOT}/_data${pathname}`);
+      if (!existsSync(filePath)) return notfoundResponse();
+    }
     const file = readFileSync(filePath)
     return new Response(file, {
       headers: {
@@ -18,11 +31,6 @@ export async function GET(req: NextRequest) {
     });
   } catch (e) {
     console.error(e);
-    return new Response("file not found", {
-      headers: {
-        "Content-Type": "text/plain",
-      },
-      status: 404
-    });
+    return notfoundResponse();
   }
 }
