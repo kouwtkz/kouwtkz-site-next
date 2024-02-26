4e6ffcdaaccb74605f8cc4d06a1dd4cd1e6d5953
kouwtkz <kouwtkz@cottonwind.com>
2024-02-26 22:30:19 +0900

    Gitの同日の重複削除と8文字未満のGit履歴の表示順を下げた

--- app/context/git/log.mjs
+++ app/context/git/log.mjs
@@ -7,17 +7,22 @@ import { execSync } from "child_process";
 
 export function getGitLog() {
   try {
-    const gitLog = execSync('git log --first-parent main --no-merges --pretty=format:"%ad,%s" --date=format:"%Y/%m/%d %H:%M:%S"')
-    const gitLogList = gitLog.toString().split("\n").map(v => v.split(","))
+    const gitLog = execSync('git log --first-parent main --no-merges --pretty=format:"%ad__,%s" --date=format:"%Y/%m/%d %H:%M:%S"')
+    const gitLogList = gitLog.toString().split("\n").map(v => v.split("__,"))
       .map(([date, message]) => ({ date: new Date(date), message }))
       .map(({ date, message }) => ({ ymd: `${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()}`, date, message }));
     /** @type { reducedGitItemType[] } */
     const gitLogReduced = [];
     gitLogList.forEach(({ ymd, message }) => {
       const found = gitLogReduced.find(a => a.date === ymd);
-      if (found) found.messages.push(message);
+      if (found) {
+        if (found.messages.every(m => m !== message)) found.messages.push(message);
+      }
       else gitLogReduced.push({ date: ymd, messages: [message] });
     });
+    gitLogReduced.forEach(log => {
+      log.messages.sort((a, b) => b.length < 8 ? -1 : 0)
+    })
     return gitLogReduced;
   } catch (e) {
     console.log(e);
