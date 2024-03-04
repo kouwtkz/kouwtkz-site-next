e42a7730b97d385913ea965d83ee9e73bd08fd24
kouwtkz <kouwtkz@cottonwind.com>
2024-03-05 01:22:20 +0900

    CharaDataFunctionに改名

--- app/data/galleryTags.json/route.ts
+++ app/data/galleryTags.json/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server"
 import isStatic from "@/app/components/System/isStatic.mjs";
 export const dynamic = isStatic ? "auto" : "force-dynamic";
 
-import { charaObject } from "@/app/character/getCharaData.mjs";
+import { charaObject } from "@/app/character/CharaDataFunction.mjs";
 
 export async function GET() {
   return NextResponse.json(charaObject);
