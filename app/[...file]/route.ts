ff1c0601f46c3c6629bd1ca2be20e661dad35eab
kouwtkz <kouwtkz@cottonwind.com>
2024-03-13 12:20:14 +0900

    メインブランチで用意した関数を適用させた

--- app/[...file]/route.ts
+++ app/[...file]/route.ts
@@ -1,35 +1,8 @@
-import isStatic from "@/app/components/System/isStatic.mjs";
+import isStatic from "@/app/context/system/isStatic.mjs";
 export const dynamic = isStatic ? "auto" : "force-dynamic";
-import { readFileSync, existsSync } from "fs";
-import { contentType } from "mime-types"
 import { NextRequest } from "next/server";
-import { extname, resolve } from "path";
-const ROOT = process.env.ROOT || ".";
-
-function notfoundResponse() {
-  return new Response("file not found", {
-    headers: {
-      "Content-Type": "text/plain",
-    },
-    status: 404
-  });
-}
+import { dataResponse } from "../context/system/ServerFunction";
 
 export async function GET(req: NextRequest) {
-  let pathname = decodeURI(req.nextUrl.pathname);
-  try {
-    let filePath =
-      [`${ROOT}/public${pathname}`, `${ROOT}/_data${pathname}`, `${ROOT}/_data/_media${pathname}`]
-        .find(path => existsSync(path));
-    if (!filePath) return notfoundResponse();
-    const file = readFileSync(resolve(filePath))
-    return new Response(file, {
-      headers: {
-        "Content-Type": contentType(extname(pathname)) || "application/octet-stream",
-      }
-    });
-  } catch (e) {
-    console.error(e);
-    return notfoundResponse();
-  }
+  return dataResponse(decodeURI(req.nextUrl.pathname));
 }
